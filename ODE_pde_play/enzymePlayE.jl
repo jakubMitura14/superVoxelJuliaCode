3e034f1864efa1a348bf97ea2e836b914f38eca6
Jakub Mitura <jakub.mitura14@gmail.com>
2022-08-18 20:14:27 +0200

    gradients seem to work look one iter
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

--- ODE_pde_play/enzymePlayE.jl
+++ ODE_pde_play/enzymePlayE.jl
@@ -51,11 +51,11 @@ function mul_kernel(Nx, Ny, Nz, A, p, Aout)
     x = (threadIdx().x + ((blockIdx().x - 1) * CUDA.blockDim_x())) + 1
     y = (threadIdx().y + ((blockIdx().y - 1) * CUDA.blockDim_y())) + 1
     z = (threadIdx().z + ((blockIdx().z - 1) * CUDA.blockDim_z())) + 1
-
-    Aout[x, y, z] = alaMax(A[x, y, z], ((1 - p[x+1, y, z]) * A[x+1, y, z])) * (1 - p[x, y, z])
-    Aout[x, y, z] = alaMax(A[x, y, z], ((1 - p[x-1, y, z]) * A[x-1, y, z])) * (1 - p[x, y, z])
-    Aout[x, y, z] = alaMax(A[x, y, z], ((1 - p[x, y+1, z]) * A[x, y+1, z])) * (1 - p[x, y, z])
-    Aout[x, y, z] = alaMax(A[x, y, z], ((1 - p[x, y-1, z]) * A[x, y-1, z])) * (1 - p[x, y, z])
+    CUDA.@cuprint("x $(x) y $(y) z $(z) curr $(A[x,y,z]) z+1 $(A[x, y, z+1] ) alamax z +1 $( alaMax(A[x, y, z], ((1 - p[x, y, z-1]) * A[x, y, z-1])) * (1 - p[x, y, z]))  \n    ")
+    # Aout[x, y, z] = alaMax(A[x, y, z], ((1 - p[x+1, y, z]) * A[x+1, y, z])) * (1 - p[x, y, z])
+    # Aout[x, y, z] = alaMax(A[x, y, z], ((1 - p[x-1, y, z]) * A[x-1, y, z])) * (1 - p[x, y, z])
+    # Aout[x, y, z] = alaMax(A[x, y, z], ((1 - p[x, y+1, z]) * A[x, y+1, z])) * (1 - p[x, y, z])
+    # Aout[x, y, z] = alaMax(A[x, y, z], ((1 - p[x, y-1, z]) * A[x, y-1, z])) * (1 - p[x, y, z])
     Aout[x, y, z] = alaMax(A[x, y, z], ((1 - p[x, y, z+1]) * A[x, y, z+1])) * (1 - p[x, y, z])
     Aout[x, y, z] = alaMax(A[x, y, z], ((1 - p[x, y, z-1]) * A[x, y, z-1])) * (1 - p[x, y, z])
 
@@ -74,22 +74,51 @@ function grad_mul_kernel(Nx, Ny, Nz, A, dA, p, dp, Aout, dAout)
 end
 
 
-Nx, Ny, Nz = 64 + 2, 64 + 2, 64 + 2
+Nx, Ny, Nz = 8 + 2, 8 + 2, 8 + 2
 
-A = CuArray(Float32.(reshape(collect(1:Nx*Ny*Nz), (Nx, Ny, Nz))))
+nums = Float32.(reshape(collect(1:8*8*8), (8, 8, 8)))
+withPad= Float32.(zeros(Nx,Ny,Nz))
+withPad[2:9,2:9,2:9]=nums
+
+A = CuArray(withPad)
 dA = similar(A)
+probs = Float32.(ones(8,8,8)).*0.1
+probs[4,:,:].=0.9
+probs[:,4,:].=0.9
+probs[:,:,4].=0.9
+withPadp= Float32.(zeros(Nx,Ny,Nz))
+withPadp[2:9,2:9,2:9]=probs
 
 
-p = CuArray(Float32.(rand(0.0:1.0, Nx, Ny, Nz)))
+p = CuArray(withPadp)
 dp = CUDA.ones(Nx, Ny, Nz)
 
 Aout = CUDA.zeros(Nx, Ny, Nz)
 dAout = CUDA.ones(Nx, Ny, Nz)
 
 dA .= 1
-@cuda threads = (4, 4, 4) blocks = (8, 8, 8) grad_mul_kernel(Nx, Ny, Nz, A, dA, p, dp, Aout, dAout)
+# @cuda threads = (4, 4, 4) blocks = (2, 2, 2) grad_mul_kernel(Nx, Ny, Nz, A, dA, p, dp, Aout, dAout)
+@cuda threads = (2, 2, 2) blocks = (1, 1, 1) grad_mul_kernel(Nx, Ny, Nz, A, dA, p, dp, Aout, dAout)
 
 print("max $(maximum(dp)) min $(minimum(dp))")
+
+
+
+numsRef = reshape(collect(1:8*8*8), (8, 8, 8))
+withPadRef= zeros(Int,Nx,Ny,Nz)
+withPadRef[2:9,2:9,2:9]=numsRef
+
+function compare(x,y,z)
+    print("in ref $(withPadRef[x,y,z]) in out int $(Int(round(Aout[x,y,z]))) float $(Aout[x,y,z])  ")
+end
+compare(3,3,3)
+# 3,3,3 54359.566
+Int(round(Aout[3,2,2]))
+withPadRef[3,2,3]
+
+
+maximum(Aout)
+
 
 # /home/jakub/julia-1.7.3-linux-x86_64/julia-1.7.3/bin/julia /media/jakub/NewVolume/projects/superVoxelJuliaCode/ODE_pde_play/enzymePlayE.jl
 #/media/jakub/NewVolume/projects/superVoxelJuliaCode/ODE_pde_play/enzymePlayE.jl
