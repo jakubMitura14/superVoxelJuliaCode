0be3d75c86a163d6677ef36f8977396ffd4478b2
Jakub Mitura <jakub.mitura14@gmail.com>
2022-08-24 18:04:31 +0200

    up
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

--- chainRulesLuxExample/kernelA.jl
+++ chainRulesLuxExample/kernelA.jl
@@ -6,9 +6,10 @@ using KernelAbstractions
 using KernelGradients
 using Zygote, Lux
 using Lux, Random
+import NNlib, Optimisers, Plots, Random, Statistics, Zygote
 
 
-
+nSize = 3
 
 # Two kernels to be called one after the other.
 @kernel function example_kernel(x,out, p)
@@ -22,9 +23,10 @@ function call_example_kernel1(x, p)
     out=similar(x)
     fill!(out, 1)
     kernel = example_kernel(CUDADevice())
+    #kernel = example_kernel(CPU())
     event = kernel(x, out, p, ndrange=size(x))
     wait(event)
-    return z
+    return out
 end
 
 
@@ -46,7 +48,7 @@ function ChainRulesCore.rrule(::typeof(call_example_kernel1), x, y)
     
         # AD call.
         gpu_kernel_autodiff = autodiff(example_kernel(CUDADevice()))
-        event = gpu_kernel_autodiff(dx, dy, dz, ndrange=4)
+        event = gpu_kernel_autodiff(dx, dy, dz, ndrange=size(x))
         
         # Return differentials of input.
         f̄ = NoTangent()
@@ -71,17 +73,13 @@ function KernelA(confA::Int)
     return KernelAstr(confA)
 end
 
-l = KernelA(2)
 
 function Lux.initialparameters(rng::AbstractRNG, l::KernelAstr)
-    return (paramsA=rand(rng, l.confA, l.confA))
+    return (paramsA=rand(rng, l.confA, l.confA), paramsB = rand(rng, l.confA, l.confA))
 end
 
 Lux.initialstates(::AbstractRNG, ::KernelAstr) = NamedTuple()
 
-
-println("Parameter Length: ", Lux.parameterlength(l), "; State Length: ",
-        Lux.statelength(l))
 
 
 # # But still recommened to define these
@@ -90,12 +88,72 @@ println("Parameter Length: ", Lux.parameterlength(l), "; State Length: ",
 # Lux.statelength(::KernelAstr) = 0
 
 function (l::KernelAstr)(x::AbstractMatrix, ps, st::NamedTuple)
-    call_example_kernel1(x, ps.paramsA)
-    return out
+    return call_example_kernel1(x, ps.paramsA)
 end
 
-
 rng = Random.default_rng()
-Random.seed!(rng, 0)
+l = KernelA(nSize)
+
+rand(rng, l.confA, l.confA)
 
 ps, st = Lux.setup(rng, l)
+
+println("Parameter Length: ", Lux.parameterlength(l), "; State Length: ",
+        Lux.statelength(l))
+
+x = randn(rng, Float32, nSize, nSize)
+x=CuArray(x)
+
+Lux.apply(l, x, ps, st) # or `l(x, ps, st)`
+
+
+
+model = Lux.Chain(KernelA(nSize),KernelA(nSize) )
+opt = Optimisers.Adam(0.03)
+
+"""
+extremely simple loss function - that just wan to decrese sumof all inputs
+"""
+function loss_function(model, ps, st, x)
+    y_pred, st = Lux.apply(model, x, ps, st)
+    return sum(y_pred), st, ()
+end
+
+tstate = Lux.Training.TrainState(rng, model, opt; transform_variables=Lux.gpu)
+vjp_rule = Lux.Training.ZygoteVJP()
+
+
+function main(tstate::Lux.Training.TrainState, vjp::Lux.Training.AbstractVJP, data::AbstractMatrix,
+    epochs::Int)
+    data = data .|> Lux.gpu
+    for epoch in 1:epochs
+        grads, loss, stats, tstate = Lux.Training.compute_gradients(vjp, loss_function,
+                                                                data, tstate)
+        @info epoch=epoch loss=loss
+        tstate = Lux.Training.apply_gradients(tstate, grads)
+    end
+    return tstate
+end
+
+tstate = main(tstate, vjp_rule, x,250)
+y_pred = Lux.cpu(Lux.apply(tstate.model, Lux.gpu(x), tstate.parameters, tstate.states)[1])
+
+
+
+
+
+
+
+
+
+
+# function generate_data(rng::Random.AbstractRNG)
+#     x = reshape(collect(range(-2.0f0, 2.0f0, 128)), (1, 128))
+#     y = evalpoly.(x, ((0, -2, 1),)) .+ randn(rng, (1, 128)) .* 0.1f0
+#     return (x, y)
+# end
+
+# rng = Random.MersenneTwister()
+# Random.seed!(rng, 12345)
+
+# (x, y) = generate_data(rng)
